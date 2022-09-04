530167277ddf47fbaf85cca981f8dff7d02df62e
SimiNeeluru <56332353+SimiNeeluru@users.noreply.github.com>
2022-09-04 00:08:39 -0400

    Added battery LED indicator - check pin def. at lines 8 and 10-12

--- silverhand-program/silverhand-one-motor/one-motor-code.ino
+++ silverhand-program/silverhand-one-motor/one-motor-code.ino
@@ -1,10 +1,10 @@
 #include <Servo.h>
 
-// **ONE MOTOR CODE - LAST UPDATED 8/3/2022** //
+// **ONE MOTOR CODE - LAST UPDATED 9/4/2022** //
 
 // **Pin Definitions** //
-#define SERVO_PIN 9
-#define MOSFET_PIN 8
+#define SERVO_PIN 11
+#define MOSFET_PIN 12
 #define MYO_PIN A0 // Pin for the myoware sensor
 // Pins for the LED components
 #define LED_PIN_R 2 // Red
